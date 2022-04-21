e17d376c8e20dabc5669c6214db92267f4daf217
danlee2002 <danlee@gatech.edu>
2022-04-21 13:04:44 -0400

    AddTwo

--- completed/AddTwo.java
+++ completed/AddTwo.java
@@ -1,36 +1,37 @@
 public class AddTwo {
 
-	public static ListNode  addTwo(ListNode l1,ListNode l2) {
-		ListNode temp = new ListNode();
-		return new ListNode();
+      public static ListNode addTwo(ListNode l1, ListNode l2) {
+            int carry = 0;
+            ListNode dummy = new ListNode(0);
+            //0 
+            ListNode curr = dummy;
 
+            while (l1 != null || l2 != null || carry == 1) {
+                  int sum = 0;
+                  if (l1 != null) {
+                        sum += l1.val;
+                        l1 = l1.next;
+                        //a -> b -> c  
+                        //before: curr = a
+                        //after: curr = b
+                  }
+                  if (l2 != null) {
+                        sum += l2.val;
+                        l2 = l2.next;
+                        //a -> b -> c  
+                        //before: curr = a
+                        //after: curr = b
+                  }
+                  sum += carry;
+                  carry = sum / 10;
+                  ListNode node = new ListNode(sum % 10);
+                  curr.next = node;
+                  curr = curr.next;
+                 
+            }
 
+            return dummy.next;
 
-
-	}
-	public static void main(String[] args) {
-		
-
-	}
-
-
-
-
-
-
-}
-class ListNode {
-      int val;
-      ListNode next;
-      ListNode(){ 
-      }
-      
-      ListNode(int val) {
-      this.val = val;
-      }
-      ListNode(int val, ListNode next) { 
-      this.val = val; 
-      this.next = next; 
       }
 
 }
